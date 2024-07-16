8c1df0cb458f2cc3ef7dc0e58d50a3ba13aceff8
R.Kemmochi <riki.kenmochi@gmail.com>
2024-07-16 18:42:42 +0900

    コード整形

--- sample1/AppRun.java
+++ sample1/AppRun.java
@@ -1,11 +1,11 @@
 package demo.sample1;
 
 public class AppRun {
-   public static void main(String[] args){
-    Rectangle rectangle = new Rectangle(50, 100);
-    double x = rectangle.getArea();
-    double y = rectangle.getPerimeter();
-    System.out.println("面積計算結果: "+x);
-    System.out.println("周囲の長さを計算した結果: "+y);
+   public static void main(String[] args) {
+      Rectangle rectangle = new Rectangle(50, 100);
+      double x = rectangle.getArea();
+      double y = rectangle.getPerimeter();
+      System.out.println("面積計算結果: " + x);
+      System.out.println("周囲の長さを計算した結果: " + y);
    }
 }

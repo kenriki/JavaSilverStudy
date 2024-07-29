f80a960786504ee8018d0bde3db942d035abbb51
R.Kemmochi <riki.kenmochi@gmail.com>
2024-07-29 13:17:11 +0900

    healthChecker | データ保持の追加

--- sample2/HealthChecker.java
+++ sample2/HealthChecker.java
@@ -1,14 +1,26 @@
 package demo.sample2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Optional;
 
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -19,12 +31,15 @@ import javafx.stage.Stage;
  * 健康管理メインクラス
  */
 public non-sealed class HealthChecker extends WeightBMI {
-	
+
 	private final String TITLE = "BMI計測ツール";
 	private final String LABEL_1 = "身長";
 	private final String LABEL_2 = "体重";
 	private final String LABEL_3 = "結果!!";
 	private final String BUTTON_1 = "計測";
+	private final String OUTPUT_FILE_NAME = "data.csv";
+	private final String comma = ",";
+	private final String ERR_MSG_1 = "【未入力エラー】必須入力欄が未入力となっています。";
 
 	public static void main(String... args) {
 		launch(args);
@@ -32,9 +47,10 @@ public non-sealed class HealthChecker extends WeightBMI {
 
 	/**
 	 * BMI計算メソッド
+	 * @throws IOException 
 	 */
 	@Override
-	protected BigDecimal getValueBMI(HealthInput input) {
+	protected BigDecimal getValueBMI(HealthInput input) throws IOException {
 		BigDecimal result;
 		// 170(cm) ÷ 100.00=1.7(m)
 		// BMI = 体重(kg) × (身長(m))の二乗
@@ -42,7 +58,49 @@ public non-sealed class HealthChecker extends WeightBMI {
 		double d = Math.floor(dHeight * 10) / 10;
 		result = BigDecimal.valueOf(input.weight() / (d));
 		result = result.setScale(2, RoundingMode.HALF_UP); //切り捨て  
+
 		return result;
+	}
+
+	/**
+	 * データ内容をCSVに書き込む
+	 * @param bmiStyleResult
+	 * @param result
+	 * @param input
+	 * @throws IOException
+	 */
+	private void saveCsvFile(String bmiStyleResult, BigDecimal result, HealthInput input) throws IOException {
+		// csvに入力値とBMI結果を保持
+		FileWriter csvWriter = new FileWriter(OUTPUT_FILE_NAME,true);
+		PrintWriter pw = new PrintWriter(new BufferedWriter(csvWriter));
+		pw.print((double) input.height() + comma);
+		pw.print((double) input.weight() + comma);
+		pw.print(bmiStyleResult + "/");
+		pw.println(result.doubleValue());
+		pw.close();
+	}
+
+	/**
+	 * カンマ区切りでCSVデータを読み込む
+	 * @param line
+	 * @return
+	 * @throws IOException 
+	 */
+	private String[] readCsvSplit() throws IOException {
+		File file = new File(OUTPUT_FILE_NAME);
+		FileInputStream input = new FileInputStream(file);
+		InputStreamReader stream = new InputStreamReader(input, "SJIS");
+		BufferedReader br = new BufferedReader(stream);
+		String line;
+
+		String[] data = null;
+		while ((line = br.readLine()) != null) {
+			// カンマで分割
+			data = line.split(comma);
+		}
+		br.close();
+		return data;
+
 	}
 
 	/**
@@ -52,6 +110,11 @@ public non-sealed class HealthChecker extends WeightBMI {
 	protected String getValueBMIStyle(BigDecimal bmi) {
 		BigDecimal diff1 = new BigDecimal(18.5); //低体重
 		BigDecimal diff2 = new BigDecimal(25); //標準体重
+
+		//		25-30未満	肥満（１度）
+		//		30-35未満	肥満（２度）
+		//		35-40未満	肥満（３度）
+		//		40以上	肥満（４度）
 
 		int result1 = bmi.compareTo(diff1);
 		int result2 = bmi.compareTo(diff2);
@@ -76,58 +139,82 @@ public non-sealed class HealthChecker extends WeightBMI {
 		arg0.setScene(s);
 		grp.getChildren().add(cvs);
 
-		// 身長、体重を入力するエリアの宣言
-		TextField text1 = new TextField();
-		TextField text2 = new TextField();
-		text1.setMaxWidth(200);
-		text2.setMaxWidth(200);
-
+		// 身長、体重を入力するエリアのインスタンス取得
+		TextField textHeightField = new TextField();
+		TextField textWeightField = new TextField();
 		// 出力用エリアの宣言
-		TextField output = new TextField();
-		output.setMaxWidth(200);
+		TextField textSokuteiResultField = new TextField();
+
+		// 前回のデータ取得して設定
+		String[] data = readCsvSplit();
+		textHeightField.setText(data[0]);
+		textWeightField.setText(data[1]);
+		textSokuteiResultField.setText(data[2]);
+
+		// 身長、体重を入力するエリアの準備
+		textHeightField.setMaxWidth(200);
+		textWeightField.setMaxWidth(200);
+		textSokuteiResultField.setMaxWidth(200);
 
 		// ラベルやボタンの配置のための宣言
-		Label name1 = new Label(LABEL_1);
-		Label name2 = new Label(LABEL_2);
-		Label name3 = new Label(LABEL_3);
-		name1.setFont(new Font(30));
-		name2.setFont(new Font(30));
-		name3.setFont(new Font(30));
-		name1.setAlignment(Pos.CENTER);
-		name2.setAlignment(Pos.CENTER);
-		name3.setAlignment(Pos.CENTER);
-		Button btn = new Button(BUTTON_1);
+		Label heightLabelName = new Label(LABEL_1);
+		Label widthLabelName = new Label(LABEL_2);
+		Label sokuteiResultName = new Label(LABEL_3);
+		heightLabelName.setFont(new Font(30));
+		widthLabelName.setFont(new Font(30));
+		sokuteiResultName.setFont(new Font(30));
+		heightLabelName.setAlignment(Pos.CENTER);
+		widthLabelName.setAlignment(Pos.CENTER);
+		sokuteiResultName.setAlignment(Pos.CENTER);
+		Button sokuteiBtn = new Button(BUTTON_1);
 
 		// 測定押下イベント
-		btn.setOnAction((ActionEvent event) -> {
-			if (text1.getText() == "" || text1.getText() == "") {
+		sokuteiBtn.setOnAction((ActionEvent event) -> {
+			if (textHeightField.getText() == "" || textHeightField.getText() == "") {
+				Alert alert = new Alert(AlertType.ERROR, ERR_MSG_1, ButtonType.OK);
+				Optional opt = alert.showAndWait();
+				if (opt.get() == ButtonType.OK) {
+					return;
+				}
 				return;
 			}
 			// TextFieldからDoubleへ変換して格納
-			double height = (Double) (Double.parseDouble(text1.getText()));
-			double width = (Double) (Double.parseDouble(text2.getText()));
+			double height = (Double) (Double.parseDouble(textHeightField.getText()));
+			double weidth = (Double) (Double.parseDouble(textWeightField.getText()));
 
 			// Recordクラスにインプット情報を保持
-			HealthInput healInput = new HealthInput(height, width);
+			HealthInput healInput = new HealthInput(height, weidth);
 
 			// BMI数値を取得する
-			BigDecimal result = this.getValueBMI(healInput);
+			BigDecimal result = null;
+			try {
+				result = this.getValueBMI(healInput);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
 			// BMIの結果を日本語で取得する
 			String bmiStyleResult = this.getValueBMIStyle(result);
-			output.setText(bmiStyleResult + "/" + result.toString());
+			textSokuteiResultField.setText(bmiStyleResult + "/" + result.toString());
+
+			// csvに入力値とBMI結果を保持
+			try {
+				saveCsvFile(bmiStyleResult, result, healInput);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		});
 
 		// 配置
 		VBox vbRoot = new VBox();
 		vbRoot.setAlignment(Pos.CENTER);
-		vbRoot.getChildren().add(name1);
-		vbRoot.getChildren().add(text1);
-		vbRoot.getChildren().add(name2);
-		vbRoot.getChildren().add(text2);
-		vbRoot.getChildren().add(btn);
-		vbRoot.getChildren().add(name3);
-		vbRoot.getChildren().add(output);
+		vbRoot.getChildren().add(heightLabelName);
+		vbRoot.getChildren().add(textHeightField);
+		vbRoot.getChildren().add(widthLabelName);
+		vbRoot.getChildren().add(textWeightField);
+		vbRoot.getChildren().add(sokuteiBtn);
+		vbRoot.getChildren().add(sokuteiResultName);
+		vbRoot.getChildren().add(textSokuteiResultField);
 
 		arg0.setTitle(TITLE);
 		arg0.setWidth(350);
@@ -136,11 +223,6 @@ public non-sealed class HealthChecker extends WeightBMI {
 
 		// ウィンドウ表示
 		arg0.show();
-
-		// 描画
-		//				GraphicsContext g = cvs.getGraphicsContext2D();
-		//				g.setFill(Color.GREEN);
-		//				g.fillRect(10, 10, 50, 50);
 
 	}
 
--- sample2/WeightBMI.java
+++ sample2/WeightBMI.java
@@ -1,11 +1,12 @@
 package demo.sample2;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 // BMI値計算クラス（抽象クラス）
 abstract sealed public class WeightBMI extends FxBase permits HealthChecker{
     // BMIを計算して返す抽象メソッド
-    protected abstract BigDecimal getValueBMI(HealthInput in);
+    protected abstract BigDecimal getValueBMI(HealthInput in) throws IOException;
 
     // BMIを計算して判定結果を返す抽象メソッド
     protected abstract String getValueBMIStyle(BigDecimal x);
